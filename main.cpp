b1516271e2c7677b19b72da5dc5ffadfd209e159
B0nam <daniiel.bonam@gmail.com>
2023-04-10 17:21:48 -0300

    Busca de palavras [QUEBRADO]

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@
 
 int controle = 0;
 char palavra_chave[25];
+char *palavra_encontrada[2] = { NULL, NULL}; //armazena a posicao inicial e a palavra encontrada
 char matriz[25][25];
 void gerarMatriz(char (*ptr_matriz)[25][25]);
 void palavrasMatriz();
 void mostrarMatriz(char (*ptr_matriz)[25][25]);
 void inserirMatriz(char (*ptr_matriz)[25][25], int eixo);
+void buscarPalavra(char (*ptr_matriz)[25][25], char palavraUsuario[25]);
 char sortearLetra();
+
 
 int main() {
 	srand(time(NULL));
@@ -20,6 +23,16 @@ int main() {
 	inserirMatriz(&matriz, 1); //eixo 1 = horizontal, 2 = vertical
 	mostrarMatriz(&matriz);
 	std::cout << "Palavra chave: " << palavra_chave << std::endl;
+
+	char palavraDigitada[25];	
+	std::cout << "Digite uma palavra: ";
+	std::cin >> palavraDigitada;
+	buscarPalavra(&matriz, palavraDigitada);
+
+	
+	std::cout << "Palavra encontrada: " << *palavra_encontrada[1] << std::endl;	
+	std::cout << "Palavra encontrada: " << *palavra_encontrada[2] << std::endl;	
+
 	return 0;
 }
 
@@ -139,3 +152,44 @@ void mostrarMatriz(char (*ptr_matriz)[25][25]) {
 			std::cout << std::endl;
 	}
 }	
+
+//busca horizontalmente e depois verticalmente na matriz
+//comprime a linha e busca a palavra na linha.
+void buscarPalavra(char (*ptr_matriz)[25][25],char palavraUsuario[25]) {
+	char linha_comprimida[26] = { '\0' };
+	char coluna_comprimida[26] = { '\0' };
+	
+	for (int t=0;t<25;t++){ //para as 25 linhas
+		for (int y=0;y<25;y++){
+			char letra = (*ptr_matriz)[t][y]; 
+			strncat(linha_comprimida, &letra, 1);
+		}
+		//busca a palavra na linha.
+		char *resultado = strstr(linha_comprimida, palavraUsuario);
+		if (resultado != NULL) {
+			//armazena a posicao inicial onde a palavra foi encontrada
+			palavra_encontrada[0] = resultado;
+			palavra_encontrada[1] = palavraUsuario;
+		}
+		//zera a linha_comprimida.
+		std::memset(linha_comprimida, 0, sizeof(linha_comprimida));
+	}
+/*
+	for (int t=0;t<25;t++){ //para as 25 colunas
+		for (int y=0;y<25;y++){
+			coluna_comprimida = strcat(coluna_comprimida, (*ptr_matriz)[y][t])
+		}
+		//busca a palavra na linha.
+		char *resultado = strstr(coluna_comprimida, palavraUsuario);
+		if (resultado != NULL) {
+			//armazena a posicao inicial onde a palavra foi encontrada
+			palavra_encontrada[0] = resultado;
+			palavra_encontrada[1] = palavraUsuario;
+		}
+		//zera a linha_comprimida.
+		std::memset(linha_comprimida, 0, 26);
+	}
+	*/
+}
+
+//comprime a coluna e busca a palavra horizontalmente
